7a531abec4eff304d98feb5a002ef47726a026b3
Victoria <victoriakarenlo@gmail.com>
2022-02-15 01:27:10 +0000

    updated to valid new restaurant rating

--- ratings.py
+++ ratings.py
@@ -1,47 +1,48 @@
 """Restaurant rating lister."""
 
-scores_file = open("scores.txt")
-
 # put your code here
 def read_ratings(file):
     new_restaurant = input("name new restaurant: ")
-    new_rating = input("new rating: ")
 
+    while True:
+        new_rating = input("new rating: ")
     
+        try:
+            int_new_rating = int(new_rating)
+            if int_new_rating < 1 or int_new_rating > 5:
+                print("Please enter valid num between 1-5")
+                continue
+            else:
+                break
+        except ValueError:
+            print("Please enter valid num between 1-5")
+            continue
+
     restaurant_file = open(file)
 
     rest_and_ratings = {}
+    rest_and_ratings[new_restaurant] = new_rating
+
     # Reads the ratings in from the file
     for line in restaurant_file:
         line = line.rstrip()
         line = line.split(":")
         restaurant_name, restaurant_rating = line
+
     # Stores them in a dictionary
         rest_and_ratings[restaurant_name] = restaurant_rating
-        rest_and_ratings[new_restaurant] = new_rating
+
     # Return the ratings in A-Z order by restaurant
 
-    sorted_dict = sorted(rest_and_ratings.items())
+    sorted_tuple = sorted(rest_and_ratings.items())
 
-    for name, rating in sorted_dict:
+    for name, rating in sorted_tuple:
         print(f"{name} is rated at {rating}.")
     
     restaurant_file.close()
 
 
 read_ratings("scores.txt")
-
-
- 
-# Modify your script so that after it reads the scores file from disk, 
-# it prompts the user for a new restaurant and rating.
-
-# The program should:
-
-# Prompt the user for a restaurant name
-
-# Prompt the user for a restaurant score
-
-# Store the new restaurant/rating in the dictionary
-
-# Print all of the ratings in alphabetical order (including the new one, of course)
+# Modify the script so that it validates the score users provide when they add a new restaurant and rating. 
+# The rating must be an integer between 1 and 5 (inclusive). 
+# If they enter something invalid, the script should prompt them again.
